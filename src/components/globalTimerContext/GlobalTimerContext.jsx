5a33aad4eb9382bdf844975da55e9f287cb8033a
AdreanRodriguez <loco_bladare@hotmail.com>
2024-10-24 07:45:47 +0200

    clock ok

--- src/components/globalTimerContext/GlobalTimerContext.jsx
+++ src/components/globalTimerContext/GlobalTimerContext.jsx
@@ -20,10 +20,10 @@ export function GlobalTimerProvider({ children }) {
       timer = setInterval(() => {
         setSeconds((prevSeconds) => {
           if (prevSeconds > 0) {
-            return prevSeconds - 10;
+            return prevSeconds - 1;
           } else if (minutes > 0) {
             setMinutes(minutes - 1);
-            return 50;
+            return 59;
           }
         });
       }, 1000);
